387897727c3665742e07638121d34e3f3ac5e76a
WB2302103 <ahamadrufayee890@gmail.com>
2025-08-20 03:32:15 +0600

    unnecessary comment deleted

--- hard.js
+++ hard.js
@@ -16,27 +16,3 @@
 
 
 
-// const sentence='I am a hard working person';
-// let reverse=''
-// // for(const letter of sentence)
-// // {
-// //     reverse=letter+reverse;
-// // }
-// // console.log(reverse);
-
-// // letter of sentece is used to reverse the sentce in loop we use reverse to change the thing  dev:in reverse it will give ved
-
-// // 2nd way  reverse
-// // let rev='';
-// // for(let i=0;i<sentence.length;i++)
-// // {
-// //     const letter=sentence[i];
-// //     rev=letter+rev;
-   
-// // }
-// //  console.log(rev);
-// //  shohoj solution for reverse
-
-// // shortcut:for reverse split is for to take space and is also used to see the whole value if it is empty string it will take the whole value serially to reverse this thin we also use reverse variable where reverse is connected to the sentece and also we use join and it is also empty do this
-// const reversed=sentence.split('').reverse().join("");
-// console.log(reversed);
